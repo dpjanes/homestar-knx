f4e5bf2635abb42a411fbce1eee4c2fc8bedb00e
David Janes <davidjanes@davidjanes.com>
2015-11-28 06:43:07 -0500

    test code

---
+++ samples/connect_boolean.js
@@ -0,0 +1,26 @@
+/*
+ *  NOTE: prefer iotdb versions
+ *
+ *  This will:
+ */
+
+"use strict";
+
+var Bridge = require('../KNXBridge').Bridge;
+
+var exemplar = new Bridge({
+    host: '192.168.80.101',
+    ip: '3671',
+    tunnel: "tcp://0.0.0.0:13671",
+});
+exemplar.discovered = function (bridge) {
+    console.log("+", "got one", bridge.meta());
+    bridge.pulled = function (state) {
+        console.log("+", "state-change", state);
+    };
+    bridge.connect({});
+    bridge.push({
+        off: true,
+    });
+};
+exemplar.discover();

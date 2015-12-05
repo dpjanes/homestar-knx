deddba5ca43de33e90723a532aea15a15bf1b6f8
David Janes <davidjanes@davidjanes.com>
2015-12-05 13:56:50 -0500

    dimmer switch

--- models/KnxDimmer.js
+++ models/KnxDimmer.js
@@ -1,5 +1,5 @@
 /*
- *  KnxSwitch.js
+ *  KnxDimmer.js
  *
  *  David Janes
  *  IOTDB
@@ -10,6 +10,39 @@ var iotdb = require("iotdb");
 
 exports.binding = {
     bridge: require('../KNXBridge').Bridge,
-    model: require('./KnxSwitch.json'),
+    model: require('./KnxDimmer.json'),
     discover: false,
+    connectd: {
+        pre_out: function (paramd) {
+            var cookd = {};
+            var max = 255; // this should be soft
+
+            if (paramd.cookd.brightness !== undefined) {
+                cookd.brightness = Math.round(paramd.cookd.brightness * max / 100);
+                if (cookd.brightness > 0) {
+                    cookd.on = true;
+                } else {
+                    cookd.on = false;
+                }
+            } else if (paramd.cookd.on !== undefined) {
+                cookd.on = paramd.cookd.on;
+                cookd.brightness = paramd.cookd.on ? max : 0;
+            }
+
+            return cookd;
+        },
+        post_in: function (paramd) {
+            var cookd = {};
+            var max = 255; // this should be soft
+
+            if (paramd.cookd.brightness !== undefined) {
+                cookd.brightness = (parseInt(paramd.cookd.brightness) * 100 / max);
+            }
+            if (paramd.cookd.on !== undefined) {
+                cookd.on = paramd.cookd.on;
+            }
+
+            return cookd;
+        },
+    },
 };

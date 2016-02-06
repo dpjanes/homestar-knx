a3386a908c439421bb1383636b617fc3f52fa72c
David Janes <davidjanes@davidjanes.com>
2016-02-06 10:27:08 -0500

    fixed up 'push' validation

--- KNXBridge.js
+++ KNXBridge.js
@@ -313,7 +313,7 @@ KNXBridge.prototype.push = function (pushd, done) {
         return;
     }
 
-    self._validate_push(pushd);
+    self._validate_push(pushd, done);
 
     logger.info({
         method: "push",

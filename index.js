f595b2f8bed9ab2f5b03c298ef7a46d862bad61e
David Janes <davidjanes@davidjanes.com>
2016-08-22 15:23:38 -0400

    remove all the 'wrap' exports that were never used

--- index.js
+++ index.js
@@ -32,8 +32,3 @@ exports.bindings = [
     require('./models/KnxSwitch').binding,
     require('./models/KnxDimmer').binding,
 ];
-
-exports.iotdb = require("iotdb");
-exports.wrap = function (name, initd) {
-    return exports.iotdb.make_wrap(name, exports.bindings, initd);
-};
